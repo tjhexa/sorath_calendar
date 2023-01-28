b5459a8d875820e8d2482d5858add4ed14883de0
tjhackz <6826808+tjhackz@users.noreply.github.com>
2023-01-29 01:15:23 +0530

    db script updtes

--- data/sorath_db.sql
+++ data/sorath_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 28, 2023 at 07:08 PM
+-- Generation Time: Jan 28, 2023 at 08:44 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -63,12 +63,13 @@ INSERT INTO `events` (`id`, `title`, `description`, `color`, `start`, `end`, `am
 (42, 'marriage', 'NA', '#008000', '2023-01-26 00:00:00', '2023-01-27 00:00:00', '0', 'final_booking', 'full_day', '0800', '0600', '2', 'riddhi', '345345345345', 'iam', '3423234234', '44', 'mkD', 'this is internal note', 'yes', 'yes', '2023-01-28 00:00:00', '2023-01-28 23:03:22', 'jay', 'tj'),
 (43, 'anniversary ', 'NA', '#40E0D0', '2023-01-28 00:00:00', '2023-01-29 00:00:00', '0', 'final_booking', 'full_day', '0800', '0600', '5.5', 'First Party4', '6546456456', 'Jay', '1234234234', '444', 'gddfdf', 'Some internal Note 2', 'no', 'no', '2023-01-28 00:00:00', '0000-00-00 00:00:00', 'jay', ''),
 (44, 'meeting', 'NA', '#0071c5', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'other', 'half_day', '0800', '0600', '22', 'Iris Valdez', 'Error et quaerat aut', 'Tempora laudantium', 'Sit at tenetur repu', '14', 'Consequat Distincti', 'Ad aut laudantium o', 'no', 'no', '2023-01-28 00:00:00', '0000-00-00 00:00:00', 'jay', ''),
-(45, 'birthday', 'NA', '#008000', '2023-01-25 00:00:00', '2023-01-26 00:00:00', '0', 'final_booking', 'half_day', '1600', '1400', '1', 'j party', '345345345', 'd party', '45645645645645', '2', 'KDF', '45645645 internal', 'no', 'no', '2023-01-28 00:00:00', NULL, 'jay', NULL),
+(45, 'birthday', 'NA', '#40E0D0', '2023-01-25 00:00:00', '2023-01-26 00:00:00', '0', 'final_booking', 'full_day', '0800', '0600', '1', 'j party', '345345345', 'd party', '45645645645645', '2', 'KDF', '45645645 internal', 'no', 'no', '2023-01-28 00:00:00', '2023-01-29 01:13:38', 'jay', 'jay'),
 (46, 'meeting', 'NA', '#0071c5', '2023-01-27 00:00:00', '2023-01-28 00:00:00', '0', 'on_hold', 'half_day', '0800', '0600', '5', '6', '7', '8', '9', '4', 'fghfgh', 'fgrtyrt', 'yes', 'no', '0000-00-00 00:00:00', '2023-01-28 01:05:03', 'jay', 'tj'),
 (47, 'birthday', 'NA', '#FF8C00', '2023-01-24 00:00:00', '2023-01-25 00:00:00', '0', 'other', 'full_day', '1600', '1400', '4', 'fghj', 'dfgh', 'dfgh', 'dfg', '4345', 'dfghdfghdfg', 'dfghdfgh', 'no', 'no', '0000-00-00 00:00:00', '2023-01-28 01:04:38', 'jay', 'tj'),
 (48, 'janoi', 'NA', '#000', '2023-01-31 00:00:00', '2023-02-01 00:00:00', '0', 'final_booking', 'half_day', '1600', '1400', '1', 'Caesar Everett', 'Laborum sed sint cor', 'Est ipsum nisi dolor', 'Sunt voluptatem Mi', '6', 'In culpa lorem quod', 'Amet a ratione saep', 'no', 'yes', '0000-00-00 00:00:00', '2023-01-28 22:59:21', 'jay', 'tj'),
 (49, 'meeting', 'NA', '#FF8C00', '2023-01-31 00:00:00', '2023-02-01 00:00:00', '0', 'final_booking', 'full_day', '0800', '0600', '7', 'Hermione Nunez', 'Velit animi adipisi', 'Voluptas ullamco non', 'Soluta incidunt do', '13', 'Iste voluptas libero', 'Ipsam ad aspernatur', 'no', 'no', '0000-00-00 00:00:00', '2023-01-28 23:05:11', 'jay', 'tj'),
-(50, 'other', 'NA', '#40E0D0', '2023-01-31 00:00:00', '2023-02-01 00:00:00', '0', 'final_booking', 'full_day', '1600', '0600', '15', 'Cheyenne Turner', 'Aute pariatur Facer', 'Ut sit error molest', 'At pariatur Soluta', '8', 'Quas totam non conse', 'Ipsa eius consequat', 'yes', 'yes', '2023-01-28 00:59:40', NULL, 'jay', NULL);
+(50, 'other', 'NA', '#40E0D0', '2023-01-31 00:00:00', '2023-02-01 00:00:00', '0', 'final_booking', 'full_day', '1600', '0600', '15', 'Cheyenne Turner', 'Aute pariatur Facer', 'Ut sit error molest', 'At pariatur Soluta', '8', 'Quas totam non conse', 'Ipsa eius consequat', 'yes', 'yes', '2023-01-28 00:59:40', NULL, 'jay', NULL),
+(51, 'meeting', 'NA', '#008000', '2023-01-18 00:00:00', '2023-01-19 00:00:00', '0', 'on_hold', 'full_day', '1600', '1400', '4', 'paltryyyyyy', '234234', 'jv', '234234', '3', 'DDD', 'dsfgdfg', 'no', 'yes', '2023-01-29 01:08:20', '2023-01-29 01:09:28', 'j2', 'jay');
 
 -- --------------------------------------------------------
 
@@ -90,7 +91,24 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`) VALUES
 (15, 'jay', 'jay@jay.com', '*B4F905776A4FA15AD76BC3223AAA21020C2950DC', '+1 (804) 726-3572'),
-(16, 'j2', 'j2@j2.com', '*8B4ECB6865732AD1A7ACFD8B77C0601C4579865B', '+1 (681) 264-1854');
+(16, 'j2', 'j2@j2.com', '*8B4ECB6865732AD1A7ACFD8B77C0601C4579865B', '+1 (681) 264-1854'),
+(17, 'Brenden Kent', 'wopuzys@mailinator.com', '*7F0C90A004C46C64A0EB9DDDCE5DE0DC437A635C', '+1 (883) 772-1254'),
+(18, 'Alyssa Stein', 'qebok@mailinator.com', '*CCE297E56371EF7DA141573F4A815B0F42E3FE9C', '+1 (892) 681-3391'),
+(19, 'Quyn Sexton', 'jibaqe@mailinator.com', '*A4B6157319038724E3560894F7F932C8886EBFCF', '+1 (883) 722-2849'),
+(20, 'Cassidy Drake', 'relyjudy@mailinator.com', '*A4B6157319038724E3560894F7F932C8886EBFCF', '+1 (451) 736-3376'),
+(21, 'Cassidy Drake', 'relyjudy@mailinator.com', '*A4B6157319038724E3560894F7F932C8886EBFCF', '+1 (451) 736-3376'),
+(22, 'Damon House', 'qipotygyge@mailinator.com', '*23AE809DDACAF96AF0FD78ED04B6A265E05AA257', '+1 (903) 812-5116'),
+(23, 'Damon House', 'qipotygyge@mailinator.com', '*23AE809DDACAF96AF0FD78ED04B6A265E05AA257', '+1 (903) 812-5116'),
+(24, 'Damon House', 'qipotygyge@mailinator.com', '*23AE809DDACAF96AF0FD78ED04B6A265E05AA257', '+1 (903) 812-5116'),
+(25, 'Damon House', 'qipotygyge@mailinator.com', '*23AE809DDACAF96AF0FD78ED04B6A265E05AA257', '+1 (903) 812-5116'),
+(26, 'Damon House', 'qipotygyge@mailinator.com', '*23AE809DDACAF96AF0FD78ED04B6A265E05AA257', '+1 (903) 812-5116'),
+(27, 'Moses Snider', 'xejyvul@mailinator.com', '*CFB33287E42A0C8FF5A06834AF80182DAB4AAA7B', '+1 (388) 842-5913'),
+(28, 'Moses Snider', 'xejyvul@mailinator.com', '*CFB33287E42A0C8FF5A06834AF80182DAB4AAA7B', '+1 (388) 842-5913'),
+(29, 'Moses Snider', 'xejyvul@mailinator.com', '*CFB33287E42A0C8FF5A06834AF80182DAB4AAA7B', '+1 (388) 842-5913'),
+(30, 'Moses Snider', 'xejyvul@mailinator.com', '*CFB33287E42A0C8FF5A06834AF80182DAB4AAA7B', '+1 (388) 842-5913'),
+(31, 'Francesca Wood', 'vuruf@mailinator.com', '*7F0C90A004C46C64A0EB9DDDCE5DE0DC437A635C', '+1 (791) 272-6706'),
+(32, 'Francesca Wood', 'vuruf@mailinator.com', '*7F0C90A004C46C64A0EB9DDDCE5DE0DC437A635C', '+1 (791) 272-6706'),
+(33, 'Francesca Wood', 'vuruf@mailinator.com', '*7F0C90A004C46C64A0EB9DDDCE5DE0DC437A635C', '+1 (791) 272-6706');
 
 --
 -- Indexes for dumped tables
@@ -116,13 +134,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
