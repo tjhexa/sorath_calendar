c00020f748a4adc3facf58e4b32e4cb09ea5e6a3
tjhackz <6826808+tjhackz@users.noreply.github.com>
2023-01-29 00:26:54 +0530

    updates

--- registration.php
+++ registration.php
@@ -5,18 +5,23 @@
 <head>
     <title>Registration</title>
 
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-
+    <!-- <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/my-login.css">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
     <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
+    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> -->
+    <style>
+       
+    </style>
 </head>
 
-<body>
+<body class="my-login-page">
 
 
 
@@ -68,7 +73,101 @@
 
     ?>
 
-    <div class="container p-3">
+    <section class="h-100">
+        <div class="container h-100">
+            <div class="row justify-content-md-center h-100">
+                <div class="card-wrapper">
+                    <div class="brand">
+                        <img src="img/sorath_logo.png" alt="logo">
+                    </div>
+                   
+                    <div class="card fat">
+                        <div class="card-body">
+                            <h4 class="card-title">Register</h4>
+                            <form method="POST" class="my-login-validation" novalidate="" action=registration.php>
+                            
+                            <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input id="name" type="text" class="form-control" name="name" value="" required
+                                        autofocus>
+                                    <div class="invalid-feedback">
+                                        Name is invalid
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="email">E-Mail Address</label>
+                                    <input id="email" type="email" class="form-control" name="email" value="" required
+                                        autofocus>
+                                    <div class="invalid-feedback">
+                                        Email is invalid
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="phone">Phone</label>
+                                    <input id="phone" type="text" class="form-control" name="phone" value="" required
+                                        autofocus>
+                                    <div class="invalid-feedback">
+                                        Phone is invalid
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label for="password">Password
+                                       
+                                    </label>
+                                    <input id="pass1" type="password" class="form-control" name="pass1" required
+                                        data-eye onChange="form.pass2.pattern=this.value">
+                                    <div class="invalid-feedback">
+                                        Password is required
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password">Confirm Password
+                                       
+                                    </label>
+                                    <input id="pass2" type="password" class="form-control" name="pass2" required
+                                        data-eye >
+                                    <div class="invalid-feedback">
+                                        Confirm Password is required & it should match with password.
+                                    </div>
+                                </div>
+
+                                
+
+                                <div class="form-group m-0">
+                                    <button type="submit" value="Save data" class="btn btn-primary btn-block">
+                                        Register
+                                    </button>
+                                </div>                              
+                                <input type="hidden" name="op" value="save" />
+
+                            </form>
+                        </div>
+                    </div>
+                    <div class="footer">
+                        Copyright &copy; 2023 &mdash; Sorath Resorts
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <script src="js/my-login.js"></script>
+
+    <!-- <div class="container p-3">
 
         <div class="panel panel-default">
             <div class="panel-heading">Registration form</div>
@@ -113,7 +212,7 @@
             </div>
         </div>
 
-    </div>
+    </div> -->
 
 </body>
 
